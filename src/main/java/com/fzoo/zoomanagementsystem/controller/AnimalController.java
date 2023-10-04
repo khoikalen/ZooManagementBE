7bfa563b0337f508124ed893fe558b6a8c6ba10b
kanatsumi <naduccva@gmail.com>
2023-10-04 15:06:36 +0700

    commit to revert 4

--- src/main/java/com/fzoo/zoomanagementsystem/controller/AnimalController.java
+++ src/main/java/com/fzoo/zoomanagementsystem/controller/AnimalController.java
@@ -1,25 +1,4 @@
 package com.fzoo.zoomanagementsystem.controller;
 
-import com.fzoo.zoomanagementsystem.dto.AnimalDTO;
-import com.fzoo.zoomanagementsystem.model.Animal;
-import com.fzoo.zoomanagementsystem.service.AnimalService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("api")
 public class AnimalController {
-    private final AnimalService animalService;
-    @GetMapping("/v1/animal")
-    public List<Animal> getListAnimals(){
-        return animalService.listOfAnimals();
-    }
-
-    @PostMapping("/v1/animal")
-    public void createNewAnimal(@RequestBody Animal animal){
-        animalService.createAnimal(animal);
-    }
 }
