84cec9496678a28e2c606ae6019c59ea9f5ecbe1
Kalen <kalen153246789@gmail.com>
2023-10-04 17:29:59 +0700

    Update Staff function and Search Staff function

--- src/main/java/com/fzoo/zoomanagementsystem/repository/StaffRepository.java
+++ src/main/java/com/fzoo/zoomanagementsystem/repository/StaffRepository.java
@@ -1,12 +1,12 @@
 package com.fzoo.zoomanagementsystem.repository;
 
-import com.fzoo.zoomanagementsystem.model.Cage;
 import com.fzoo.zoomanagementsystem.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
@@ -14,5 +14,8 @@ public interface StaffRepository extends JpaRepository<Staff, Integer> {
 
     boolean existsByPhoneNumber(String phoneNumber);
 
+    Optional<Staff> findStaffById(int staffId);
 
+    @Query("SELECT s FROM Staff s WHERE s.firstName LIKE %?1% OR s.lastName LIKE %?1%")
+    List<Staff> findStaffByName(String search);
 }
