20c4805e53b7c1af79e984722c4a9a78af418dfa
kanatsumi2003 <naduccva@gmail.com>
2023-10-17 16:35:34 +0700

    Enable Cors to get api

---
+++ src/main/java/com/fzoo/zoomanagementsystem/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.fzoo.zoomanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access=Control-Allow-Headers");
+    }
+}
