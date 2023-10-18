50eca67d99bcd042c862fe09f7fb01833aa63ef3
kanatsumi2003 <naduccva@gmail.com>
2023-10-18 20:56:25 +0700

    AnimalSpecies (not official) to modify change database configuration in application.properties

---
+++ src/main/java/com/fzoo/zoomanagementsystem/controller/AnimalSpeciesController.java
@@ -0,0 +1,18 @@
+package com.fzoo.zoomanagementsystem.controller;
+
+import com.fzoo.zoomanagementsystem.model.AnimalSpecies;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class AnimalSpeciesController {
+//    private final an
+//    @GetMapping
+//    public List<AnimalSpecies> getAllAnimalSpecie(){
+//        return List.of();
+//    }
+}
